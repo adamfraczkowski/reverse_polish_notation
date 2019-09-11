59897a13b1dd02591f20d34bde26fa8eec23c425
Adam <adam.fraczkowski@gmail.com>
2019-09-11 20:20:06 +0200

    simple http server

--- go-worker/src/compute-rpn/main.go
+++ go-worker/src/compute-rpn/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func computeRPN(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/compute", computeRPN).Methods("POST")
+
+	http.ListenAndServe(":8080", r)
 }
