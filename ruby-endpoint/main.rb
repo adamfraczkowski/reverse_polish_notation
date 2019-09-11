e4352bca35f3f629774c82a596d045f057cd5fa2
Adam <adam.fraczkowski@gmail.com>
2019-09-11 23:28:11 +0200

    Ruby data gathering in progress

--- ruby-endpoint/main.rb
+++ ruby-endpoint/main.rb
@@ -1,4 +1,21 @@
 #!/usr/bin/ruby
-puts "Write text:"
-input_string = $stdin.gets
-puts input_string
+require 'json'
+
+QUEUE_SERVER = "http://localhost:8081/queue"
+
+# example info message
+expression_array = []
+puts "Put number of expressions and expressions, separated by \\n:"
+input_num = $stdin.gets.chomp
+# Check number of inputs
+exp_num = input_num.to_i
+# Get expressions
+(1..exp_num).each { |i|
+    temporary_input = $stdin.gets.chomp
+    expression_array << temporary_input
+}
+#prepare json struct
+jsonStruct = JSON[expression_array]
+#send data to queue server
+result_of_queue_request = HTTParty.post(QUEUE_SERVER,:body => jsonStruct,:headers => {'Content-Type' => 'application/json'})
+#parsing sorted result to output
