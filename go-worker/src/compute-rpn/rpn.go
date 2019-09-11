a898c625fb42d262dba541f11ff39052b6e59dfd
Adam <adam.fraczkowski@gmail.com>
2019-09-11 20:13:26 +0200

    short optimalization, test passed. Final algorithm to RPN

--- go-worker/src/compute-rpn/rpn.go
+++ go-worker/src/compute-rpn/rpn.go
@@ -1,33 +1,89 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // RPN calculation engine struct
 type RPN struct {
 	//stack storage
-	stack []string
+	stack []int
 }
 
 // Pop from stack
-func (m *RPN) Pop() string {
+func (m *RPN) Pop() (int, error) {
 	if len(m.stack) <= 0 {
-		return ""
+		return -1, fmt.Errorf("Stack empty")
 	}
 	popValue := m.stack[len(m.stack)-1]
 	m.stack = m.stack[:len(m.stack)-1]
-	return popValue
+	return popValue, nil
 }
 
 // Push to stack
-func (m *RPN) Push(element string) string {
+func (m *RPN) Push(element int) int {
 	m.stack = append(m.stack, element)
 	return element
 }
 
 // GetStack Get actual stack
-func (m *RPN) GetStack() []string {
+func (m *RPN) GetStack() []int {
 	return m.stack
 }
 
 // StackLen get actual elements count in stack
 func (m *RPN) StackLen() int {
 	return len(m.stack)
+}
+
+// CalcRPN calculate RPN expression
+func (m *RPN) CalcRPN(expression string) (int, error) {
+	// split string into slice
+	expressionSlice := strings.Split(expression, " ")
+	for i := 0; i < len(expressionSlice); i++ {
+		switch expressionSlice[i] {
+		case "*":
+			valA, _ := m.Pop()
+			valB, _ := m.Pop()
+			result := valB * valA
+			m.Push(result)
+			break
+
+		case "+":
+			valA, _ := m.Pop()
+			valB, _ := m.Pop()
+			result := valB + valA
+			m.Push(result)
+			break
+
+		case "-":
+			valA, _ := m.Pop()
+			valB, _ := m.Pop()
+			result := valB - valA
+			m.Push(result)
+			break
+
+		case "/":
+			valA, _ := m.Pop()
+			valB, _ := m.Pop()
+			result := valB / valA
+			m.Push(result)
+			break
+
+		default:
+			// simple validation of phrase
+			_, err := strconv.Atoi(expressionSlice[i])
+			if err != nil {
+				return -1, fmt.Errorf("Incorrect phrase in expression %s", expressionSlice[i])
+			}
+			//add value to stack
+			val, _ := strconv.Atoi(expressionSlice[i])
+			m.Push(val)
+			break
+		}
+	}
+	finalResult, _ := m.Pop()
+	return finalResult, nil
 }
--- go-worker/src/compute-rpn/rpn_test.go
+++ go-worker/src/compute-rpn/rpn_test.go
@@ -6,9 +6,9 @@ func TestStack(t *testing.T) {
 	// initialize stack
 	testStack := RPN{}
 	// push values to stack
-	_ = testStack.Push("1")
-	_ = testStack.Push("2")
-	_ = testStack.Push("3")
+	_ = testStack.Push(1)
+	_ = testStack.Push(2)
+	_ = testStack.Push(3)
 	// check length of stack
 	stackLength := testStack.StackLen()
 	if stackLength != 3 {
@@ -19,13 +19,13 @@ func TestStack(t *testing.T) {
 	if len(stackList) != 3 {
 		t.Errorf("GetStack method failed got %d, want %d", len(stackList), 3)
 	}
-	if stackList[0] != "1" || stackList[1] != "2" || stackList[2] != "3" {
+	if stackList[0] != 1 || stackList[1] != 2 || stackList[2] != 3 {
 		t.Errorf("Incorrect order of stack")
 	}
 	// pop stack and check value and stack length after pop
-	testValue := testStack.Pop()
-	if testValue != "3" {
-		t.Errorf("Incorrect value from POP got %s want %s", testValue, "3")
+	testValue, _ := testStack.Pop()
+	if testValue != 3 {
+		t.Errorf("Incorrect value from POP got %d want %s", testValue, "3")
 	}
 	//checking length after pop
 	actualLen := testStack.StackLen()
@@ -33,18 +33,38 @@ func TestStack(t *testing.T) {
 		t.Errorf("Incorrect stack length got %d want %d", actualLen, 2)
 	}
 	// pop two times to have zero length
-	_ = testStack.Pop()
-	_ = testStack.Pop()
+	_, _ = testStack.Pop()
+	_, _ = testStack.Pop()
 	stackLen := testStack.StackLen()
 	if stackLen != 0 {
 		t.Errorf("Incorrect stack length got %d want %d", stackLen, 0)
 	}
-	actualZeroVal := testStack.Pop()
-	if actualZeroVal != "" {
-		t.Errorf("Incorrect stack value got %s want %s", actualZeroVal, "")
+	actualZeroVal, err := testStack.Pop()
+	if err == nil {
+		t.Errorf("Incorrect stack value got %d want %s", actualZeroVal, "")
 	}
-}
+	//check RPN Value
+	result, err := testStack.CalcRPN("12 2 3 4 * 10 5 / + * +")
+	if err != nil {
+		t.Errorf("Error during calculation.%s", err)
+	}
+	if result != 40 {
+		t.Errorf("Incorrect result value got %d want %s", result, "40")
+	}
 
-func TestRPN(t *testing.T) {
+	resultSec, errSec := testStack.CalcRPN("3 4 +")
+	if errSec != nil {
+		t.Errorf("Error during calculation.%s", err)
+	}
+	if resultSec != 7 {
+		t.Errorf("Incorrect result value got %d want %s", result, "40")
+	}
 
+	resultTh, errTh := testStack.CalcRPN("5 1 2 + 4 * + 3 -")
+	if errTh != nil {
+		t.Errorf("Error during calculation.%s", err)
+	}
+	if resultTh != 14 {
+		t.Errorf("Incorrect result value got %d want %s", result, "14")
+	}
 }
